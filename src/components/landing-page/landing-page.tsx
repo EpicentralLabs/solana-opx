6d8379f800a084ffcf3328b556bf27be308fc5ef
TheLazySol <tbelonog25@gmail.com>
2025-05-20 09:25:17 -0400

    Update version to 0.8.0 and enhance landing page functionality
    - Bumped the version in package.json to 0.8.0.
    - Added routing functionality to the landing page buttons, enabling navigation to specific paths for trading, options learning, and liquidity provision.
    - Improved user experience by disabling buttons when no path is available.

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -3,6 +3,7 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 const additionalResources = [
   {
@@ -20,26 +21,32 @@ const additionalResources = [
 ]
 
 export default function DashboardFeature() {
+  const router = useRouter()
+  
   const cards = [
     {
       title: "Trade More for Less",
       description: "One contract represents 100x of the underlying asset.",
-      buttonText: "Trade Now"
+      buttonText: "Trade Now",
+      path: "/trade"
     },
     {
       title: "Hedging Strategies", 
       description: "Allow for more strategic hedging opportunities to help mitigate risks.",
-      buttonText: "Learn Options"
+      buttonText: "Learn Options",
+      path: null
     },
     {
       title: "Market Making",
       description: "Short-sell options on margin and collect premiums every 400ms.",
-      buttonText: "Mint Options"
+      buttonText: "Mint Options",
+      path: "/option-lab"
     },
     {
       title: "Earn Yield",
       description: "Provide liquidity to the Option Margin Liquidity Pool (OMLP), earn yield.",
-      buttonText: "Provide Liquidity"
+      buttonText: "Provide Liquidity",
+      path: "/omlp"
     }
   ]
 
@@ -90,6 +97,8 @@ export default function DashboardFeature() {
                 className="w-full bg-transparent border border-[#e5e5e5]/50 dark:border-[#393939]
                   hover:bg-gray-900 hover:scale-95
                   transition-all duration-200"
+                onClick={() => card.path && router.push(card.path)}
+                disabled={!card.path}
               >
                 {card.buttonText}
               </Button>
